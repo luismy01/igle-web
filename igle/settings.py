838f35688bbf2b6e32a5d9d3da9b124b4d335929
Luis <luismy01@gmail.com>
2015-10-12 12:34:26 -0500

    Configuracion del entorno para dotcloud.com (archivos estaticos), parte 4

--- igle/settings.py
+++ igle/settings.py
@@ -108,8 +108,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-
+# STATIC_URL = '/static/'
+STATIC_URL = 'http://igle.alwaysdata.net/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
