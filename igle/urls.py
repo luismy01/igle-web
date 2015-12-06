cd2fca6ee90dd1801e9be2240a64665a5a94c5af
Luis Miguel <luismy01@gmail.com>
2015-12-06 08:49:28 -0500

    Prueba Geolocation API

--- igle/urls.py
+++ igle/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import TemplateView
 
 urlpatterns = [
     
@@ -25,6 +26,7 @@ urlpatterns = [
 	url(r'^personas/', include('personas.urls', namespace="personas", app_name="personas")),
 	
 	url(r'^login/$',  login, {"template_name": "igle/login.html"}, name="login"),
-    
 	url(r'^logout/$', logout, {"next_page": reverse_lazy("home")}, name="logout"),
+	
+	url(r'^xp/map/$', TemplateView.as_view(template_name="xp/map.html"), name="xp_map"),
 ]
